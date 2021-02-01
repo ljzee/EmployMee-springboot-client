eb78dc2ed126b057cc98ff04b2a7be5eda505e48
Louis <ljbzhuo@gmail.com>
2021-01-31 18:55:54 -0800

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+              test: /\.css$/i,
+              use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'file-loader',
+                options: {
+                  esModule: false
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
+        }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: (process.env.NODE_ENV === 'production') ? 'https://boiling-sands-12792.herokuapp.com' : 'http://localhost:8080',
+        })
+    }
+}
